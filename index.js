fe9e7aa0d515d824eadc17e2941bebf12cd4bacb
Ilya Plyusnin <iplyusnin@NMDPS.FRSP.LOCAL>
2024-01-23 13:41:08 -0700

    Adjust formatting

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 
 dotenv.config();
 
-const iprRoute = require('./routes/iprs');
+const iprRoute = require("./routes/iprs");
 
 app.use(express.json());
 app.use(helmet());
@@ -27,17 +27,17 @@ app.use(
 app.use("/api/ipr", iprRoute);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to my site");
-  });
-  
-  mongoose.connect(process.env.MONGO_URL).then(
-    () => {
-      console.log("Connected to MongoDB");
-      app.listen(8800, () => {
-        console.log("Backend server is runnng");
-      });
-    },
-    (err) => {
-      console.log(err);
-    }
-  );
+  res.send("Welcome to my site");
+});
+
+mongoose.connect(process.env.MONGO_URL).then(
+  () => {
+    console.log("Connected to MongoDB");
+    app.listen(8800, () => {
+      console.log("Backend server is runnng");
+    });
+  },
+  (err) => {
+    console.log(err);
+  }
+);
